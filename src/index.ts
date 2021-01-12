e7f9ee72b4c5d88b8e8b87e0dfaf2fdd6d351c41
Benjamin D. Sanfelippo <benjamin.d.sanfelippo@jci.com>
2021-01-12 14:09:30 -0600

    feat: add uppercase api
    this is just a simple example

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,16 @@
  *     permitted in writing by Johnson Controls, is prohibited.
  */
 
-// TODO - replace this code with your own
-
+/**
+ * A sample function that returns a name.
+ */
 export function getName() {
     return "dev-lunch-1";
+}
+
+/**
+ * A sample function that returns a name in all caps.
+ */
+export function getNameUppercase() {
+    return getName().toUpperCase();
 }
